4a9cf306623e2a0f6eb8dcfb6ee6c69cebc55fde
Onimeshnik <50239878+Onimeshnik@users.noreply.github.com>
2020-09-21 19:39:15 +0600

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,27 +1,27 @@
 exports.run = async (client, message, args, ops) => {
-    let fetched = ops.active.get(message.guild.id);
-
-    if (!fetched) return message.channel.send(`В очереди ничего нет!`);
-
-    if(message.member.voice.channel !== message.guild.me.voice.channel) return message.channel.send(`Вы должны находиться в одном канале с ботом.`);
-
-    let userCount = message.member.voice.channel.members.size;
-
-    let required = Math.ceil(userCount/2);
-
-    if (!fetched.queue[0].voteSkips) fetched.queue[0].voteSkips = [];
-
-    if (fetched.queue[0].voteSkips.includes(message.member.id)) return message.channel.send(`Вы уже проголосовали!`);
-
-    fetched.queue[0].voteSkips.push(message.member.id);
-
+    let fetched = ops.active.get(message.guild.id)
+    if (!fetched) return message.channel.send("Бот не в голосом канале!")
+    if(message.guild.me.voice.channelID !== message.member.voice.channelID) return message.channel.send(`Вы должны находиться в одном канале с ботом.`);
     ops.active.set(message.guild.id, fetched);
+    fetched.queue.shift()
 
-    if (fetched.queue[0].voteSkips.lenght >= required) {
-        message.channel.send(`Пропущено`);
+    if (fetched.queue.length > 0) {
+        async function play(client, ops, data) {
 
-        return fetched.dispatcher.emit(`finish`);
+
+            const ytdl = require("ytdl-core")
+            client.channels.cache.get(data.queue[0].announceChannel).send(`Пропущен трек ${data.queue[0].songTitle}, добавленный ${data.queue[0].requester}`)
+            data.dispatcher = await data.connection.play(ytdl(data.queue[0].url, { filter: 'audioonly' }))
+            data.dispatcher.guildID = data.guildID
+            data.dispatcher.once('finish', function () {
+                finish(client, ops, this)
+            })
+        }
+        ops.active.set(fetched.dispatcher.guildID, fetched)
+        play(client, ops, fetched)
+    } else {
+        ops.active.delete(fetched.dispatcher.guildID)
+        let vc = client.guilds.cache.get(fetched.dispatcher.guildID).me.voiceChannel
+        if (vc) vc.leave()
     }
-
-    message.channel.send(`Успешно пропущено! ${fetched.queue[0].voteSkips.lenght}/${required} необходимо.`);
 }
