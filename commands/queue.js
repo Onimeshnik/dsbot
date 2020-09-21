8425d801deccebbb91c749d755c93e402eeaa545
Onimeshnik <50239878+Onimeshnik@users.noreply.github.com>
2020-09-21 19:39:32 +0600

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -1,3 +1,5 @@
+const Discord = require(`discord.js`);
+
 exports.run = async(client, message, args, ops) => {
     
     let fethed = ops.active.get(message.guild.id);
@@ -7,11 +9,29 @@ exports.run = async(client, message, args, ops) => {
     let queue = fethed.queue;
     let nowPlaying = queue[0];
 
-    let resp = `Сейчас играет: ${nowPlaying.songTitle} | Добавил: ${nowPlaying.requester}`;
-
-    for(var i = 1; i < queue.lenght; i++) {
-        resp += `${i}. ${queue[i].songTitle} | Добавил: ${queue[i].requester}`;
+    let q = ``;
+    for(var i = 1; i < queue.length; i++) {
+        q += `\n${i}. **${queue[i].songTitle}**\nДобавил: ${queue[i].requester}`;
     }
-
-    message.channel.send(resp);
+    let resp = [
+        {name: `Сейчас играет: `, value: nowPlaying.songTitle},
+        {name: `Добавил: `, value: nowPlaying.requester},
+        {name: `Следующее`, value: q},
+    ];
+    let resp2 = [
+        {name: `Сейчас играет: `, value: nowPlaying.songTitle},
+        {name: `Добавил: `, value: nowPlaying.requester},
+    ];
+    
+    //Putting it all together
+    if(i>=2){
+        message.channel.send({embed: {
+            title: 'Очередь',
+            fields: resp,
+    }})}
+    else{
+        message.channel.send({embed: {
+            title: 'Очередь',
+            fields: resp2,
+    }})};
 }
